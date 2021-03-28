3ce97fa196f0a037f604566ceb8853fd68fab392
Md Nazmus Sakib Nayem <68137240+Nayem111@users.noreply.github.com>
2021-03-29 00:16:29 +0600

    first

---
+++ practice.js
@@ -0,0 +1,283 @@
+/*
+let js = 'awesome';
+console.log(js);
+console.log(7-11+2+4-10);
+
+let firstName='nazmus';
+let LastName='sakib nayem';
+console.log(firstName+' '+LastName + ' is an it insturctor and enterpreneur' + firstName+' founded nbict lab ');
+
+let vName= 11;
+let PI=3.14;
+console.log(PI);
+
+let x=10;
+console.log(typeof(x));
+console.log(typeof(firstName));
+console.log(typeof(null));
+*/
+
+/*let age = 30;
+age = 31;
+console.log(age);
+const birthYear = 1990;
+console.log(birthYear);
+let myName;
+console.log(myName);
+/*const yr;
+console.log(yr);*/
+/* var should not use
+var sub="stat";
+console.log(sub);*/
+
+/*myName='Nazmus Sakib';
+console.log(myName);*/
+
+//mathmatical operator//
+/*const now=2021;
+const nayem= now-1998;
+
+const davidAge=now-1990;
+
+console.log(nayem,davidAge);
+
+console.log(nayem*2,nayem/10, 2**3);
+
+const firstName='nbict';
+const LastName='lab';
+
+console.log(firstName + ' ' + LastName);
+
+// assingment operator//
+let x = 7+11;
+x+=3; //x=x+3//
+x-=4; //x=x-4//
+x++; //x=x+1//
+x--; //x=x-1//
+x--; //x=x-1//
+console.log(x);
+
+//comparison operator//
+console.log(nayem>davidAge);
+console.log(davidAge>=18);
+*/
+
+//exam//
+/*const country='bangladesh';
+const continent='asia';
+let population=160;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+const isIsland=true;
+const language='bangla';
+console.log(isIsland);
+console.log(language);
+
+console.log(typeof(isIsland));
+console.log(typeof(population));
+console.log(typeof(country));
+console.log(typeof(language));
+
+console.log(language);
+
+
+console.log(population/2);
+console.log(++population);
+
+let finland=6;
+console.log(population>finland);
+
+let averagePopulation=33;
+console.log(population<averagePopulation);
+
+let description= country+ ' '+ 'is' + ' ' + 'in' + continent + ',' + 'it' + ' ' + population +' '+ 'people' + ' '+ 'speak' + ' '+ language;
+console.log(description);
+
+
+//operator precedence
+
+const now= 2000;
+const aa=now-1990;
+const bb=now-1980;
+const average= (aa+bb)/2;
+
+console.log(aa>bb);
+
+const x=y=25-10+5;
+console.log(y);
+console.log(x);
+console.log(aa,bb,average);
+
+//test data
+ const massMark= 44;
+ const massJohn=99;
+ const heightMark=1.78;
+ const heightJohn=1.67;
+
+ const bmiMark= massMark/(heightMark*massMark);
+ const bmiJohn= massJohn/(heightJohn*massJohn);
+
+ const markHigherbmi= bmiMark>bmiJohn;
+
+ console.log(bmiMark, bmiJohn, markHigherbmi);
+
+ //strings and template literals
+
+ const firstname = 'jonas';
+ const job = 'teacher';
+ const birthyear= 1993;
+ const currentyear= 2021;
+// i am jonas, a 28 years old teacher
+// `` this is back tick symbol
+ const jonas = "I'm "+ firstname+', a '+ (currentyear-birthyear)+' years old '+job+'!';
+
+const jonasNew=`I'm ${firstname} a, ${currentyear-birthyear} years old ${job}`;
+
+console.log(jonas);
+console.log(jonasNew);
+
+console.log('I am \n\a multiline\n\ string');
+
+let z='I am\n\ james\n\ bond';
+console.log(z);
+console.log(`i am 
+a 
+multiline
+string...`);
+
+let z1=`i am
+a 
+multiline
+string`;
+ console.log(z1);
+
+ console.log(`ndbvuvjruirhnuvir`);
+
+ I am (Nayem) (sakib), I am learning (java Script). at (nbict lab) for (five) years! 
+ apply for both cotation and backtick*/
+
+
+ /*let fName = `Nazmus Sakib`;
+ let lName = `Nayem`;
+ let topic = `JavaScript`;
+ let institite = `NBICT LAB`;
+ let  StartCourse = 2015;
+ let endCourse = 2020;
+
+ console.log(`i am ${fName} ${lName}, 
+ i am leaning ${topic}. 
+ at ${institite} for ${endCourse-StartCourse} years !`);
+
+ console.log('i am '+fName +' '+lName+' , '+'\n\i am learning'+topic+'. at '+institite+' for\n\ '+ (endCourse-StartCourse)+' years !');
+
+const country=`Bangladesh`;
+let population= 160;
+const continent= `asia`;
+ console.log(country,population,continent);
+
+ const isIsland= true;
+let language=`bangla`;
+
+ console.log(`type of the variable
+  is ${typeof(country)}`);
+console.log(`type of population is
+${typeof(population)}`);
+console.log(`type of Isisland is
+${typeof(isIsland)}`);
+console.log(`type of population is
+${typeof(language)}`);
+
+
+
+console.log(language);
+
+let firstHalf=population/2;
+let secondHalf=population-firstHalf;
+console.log(`each half will contain ${secondHalf} million people`);
+
+let newPop=population+1;
+console.log(`if we add 1 to the ${population} the we will get s${newPop}`);
+
+let finPop=161;
+let condiition=population>finPop;
+if (condiition){
+    console.log(`our population is greater then Finland`)
+}else{
+    console.log(`out population is less then finalnd population`)
+};
+let averagePop=33;
+if(population>averagePop){
+    console.log(`my country is more pop than the average`)
+}else{
+    console.log(`my country is high density`)
+};
+
+let description=(`${country} is in ${continent}, and it's ${population} million speaks in ${language}`);
+console.log(description);
+
+let age=10;
+let year;
+if (age>=18){
+    console.log(`she is eligible for license`);
+}else{
+     year=18-age;
+    console.log(`apply after ${year} year(s)`) //here year could be a varible if it had let//
+};
+
+//Challange 1//
+
+ let MarkMass=78;
+ let MarkHeight=1.69;
+
+ let JohnMass=95;
+ let JohnHeight=1.88;
+
+
+ let MarkBMI=MarkMass/MarkHeight**2;
+console.log(MarkBMI);
+
+let JohnBMI=JohnMass/JohnHeight**2;
+console.log(JohnBMI);
+
+markHigherBMI=MarkBMI>JohnBMI;
+console.log(markHigherBMI);
+
+//challange 2//
+
+if (MarkBMI>JohnBMI){
+    console.log(`Mark's BMI is higher than John's`)
+}else{
+    console.log(`John's BMI is higher than Mark`)
+};
+
+console.log(`Mark's BMI ${MarkBMI} is higher than John's ${JohnBMI}`);*/
+
+let DolphinFirstScore = Number(prompt(`Dolphin First Score`));
+let DolphinSecondScore = Number(prompt(`Dolphin Second Score`));
+let DolphinThirdScore = Number(prompt(`Dolphin Third Score`));
+
+let KoalaFirstScore = Number(prompt(`Koala First Score`));
+let KoalaSecondScore = Number(prompt(`Koala Second Score`));
+let KoalaThirdScore = Number(prompt(`Koala Third Score`));
+
+let dolphinaveragescore = ((DolphinFirstScore + DolphinSecondScore + DolphinThirdScore) / 3);
+let Koalaaveragescore=((KoalaFirstScore+KoalaSecondScore+KoalaThirdScore)/3);
+
+console.log(`Dolphin Average Score is ${dolphinaveragescore}`);
+console.log(`Koalas Average score is ${Koalaaveragescore}`);
+
+if((dolphinaveragescore>Koalaaveragescore) && (dolphinaveragescore>=100)){
+    console.log(`Dolphin is winner`);
+} else if((Koalaaveragescore>dolphinaveragescore) && Koalaaveragescore>=100){
+    console.log(`koala is winner`);
+}else if((Koalaaveragescore === dolphinaveragescore) && (dolphinaveragescore>=100) && (Koalaaveragescore>=100)){
+    console.log(`the match is draw`);
+}else{
+    console.log(`no one is winner`);
+};
+console.log(`show the message`);
+
+
+
