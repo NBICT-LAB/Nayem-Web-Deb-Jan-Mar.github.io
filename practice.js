5dadcde457048f71cfacbc6d80d982596325253a
Md Nazmus Sakib Nayem <68137240+Nayem111@users.noreply.github.com>
2021-04-04 18:05:50 +0600

    tarnary operator

--- practice.js
+++ practice.js
@@ -331,8 +331,8 @@ const age = 30;
 
 //age >= 18 ? console.log(`I like to drink wine!`) : console.log(`I like to drink water!`);
 
-const drink = age >= 18 ? 'wine' : 'water' ;
-console.log(`${drink}`);
+//const drink = age >= 18 ? 'wine' : 'water' ;
+//console.log(`${drink}`);
 
 /*if (age >=18){
    drink='wine'
@@ -340,4 +340,6 @@ console.log(`${drink}`);
    drink='water'
 };
 console.log(`${drink}`);*/
+
+console.log(`I like to drink ${age>=18 ? 'wine' : 'water'}.`);
 
